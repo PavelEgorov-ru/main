b2892b7c22ac5d8cb01a452f1f3deb7b6ec77200
PavelEgorov-ru <pkotofffff@yandex.ru>
2024-10-24 00:07:48 +0500

    не работает переключение терминала

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -38,6 +38,7 @@ require("lazy").setup({
     	}
     },
     { "nvim-treesitter/nvim-treesitter" },
+    {'akinsho/toggleterm.nvim', version = "*", config = true},
     {'joshdick/onedark.vim'},
     {'cocopon/iceberg.vim'},
     {'AlessandroYorba/Alduin'},
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,4 @@
+require("toggleterm").setup{
+  open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+  shell = vim.o.shell,
+}
